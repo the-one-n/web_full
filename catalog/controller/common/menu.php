d9986ce7ab5a6184a0c7fc0530b5c8fa940874d1
pvrvd0x <d3m35s1ah@protonmail.com>
2021-09-22 22:59:05 +0300

    СУКА БЛЯТЬ КАК ЖЕ Я ЗАЕБАЛСЯ НАХУЙ УЖЕ

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -43,9 +43,46 @@ class ControllerCommonMenu extends Controller {
                             $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
                         }
 
+                        $options = array();
+
+                        foreach ($this->model_catalog_product->getProductOptions($product['product_id']) as $option) {
+                            $product_option_value_data = array();
+
+                            foreach ($option['product_option_value'] as $option_value) {
+                                if (!$option_value['subtract'] || ($option_value['quantity'] > 0)) {
+                                    if ((($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) && (float)$option_value['price']) {
+                                        $price = $this->currency->format($this->tax->calculate($option_value['price'], $product['tax_class_id'], $this->config->get('config_tax') ? 'P' : false), $this->session->data['currency']);
+                                    } else {
+                                        $price = false;
+                                    }
+
+                                    $product_option_value_data[] = array(
+                                        'product_option_value_id' => $option_value['product_option_value_id'],
+                                        'option_value_id'         => $option_value['option_value_id'],
+                                        'name'                    => $option_value['name'],
+                                        'image'                   => $this->model_tool_image->resize($option_value['image'], 50, 50),
+                                        'price'                   => $price,
+                                        'price_prefix'            => $option_value['price_prefix']
+                                    );
+                                }
+                            }
+
+                            $options[] = array(
+                                'product_option_id'    => $option['product_option_id'],
+                                'product_option_value' => $product_option_value_data,
+                                'option_id'            => $option['option_id'],
+                                'name'                 => $option['name'],
+                                'type'                 => $option['type'],
+                                'value'                => $option['value'],
+                                'required'             => $option['required']
+                            );
+                        }
+
                         $products[] = array(
+                            'id' => $product['product_id'],
                             'thumb' => $image,
                             'name' => $product['name'],
+                            'options' => $options,
                             'href' => $this->url->link('product/product', 'product_id=' . $product['product_id']),
                             'price'      => $this->currency->format($product['price'], $this->config->get('config_currency')),
                         );
---
+++ catalog/view/javascript/custom/add-to-cart.js
@@ -0,0 +1,54 @@
+function addToCart(e) {
+    const $e = $(e);
+
+        $.ajax({
+            url: 'index.php?route=checkout/cart/add',
+            type: 'post',
+            data: $e.siblings('input[type=\'text\'], input[type=\'hidden\'], input[type=\'radio\']:checked, input[type=\'checkbox\']:checked, select, textarea'),
+            dataType: 'json',
+            beforeSend: function() {
+                $e.button('loading');
+            },
+            complete: function() {
+                $e.button('reset');
+            },
+            success: function(json) {
+                $('.alert-dismissible, .text-danger').remove();
+                $('.form-group').removeClass('has-error');
+
+                if (json['error']) {
+                    if (json['error']['option']) {
+                        for (i in json['error']['option']) {
+                            var element = $('#input-option' + i.replace('_', '-'));
+
+                            if (element.parent().hasClass('input-group')) {
+                                element.parent().after('<div class="text-danger">' + json['error']['option'][i] + '</div>');
+                            } else {
+                                element.after('<div class="text-danger">' + json['error']['option'][i] + '</div>');
+                            }
+                        }
+                    }
+
+                    if (json['error']['recurring']) {
+                        $('select[name=\'recurring_id\']').after('<div class="text-danger">' + json['error']['recurring'] + '</div>');
+                    }
+
+                    // Highlight any found errors
+                    $('.text-danger').parent().addClass('has-error');
+                }
+
+                if (json['success']) {
+                    $('.breadcrumb').after('<div class="alert alert-success alert-dismissible">' + json['success'] + '<button type="button" class="close" data-dismiss="alert">&times;</button></div>');
+
+                    $('#cart > button').html('<span id="cart-total"><i class="fa fa-shopping-cart"></i> ' + json['total'] + '</span>');
+
+                    $('html, body').animate({ scrollTop: 0 }, 'slow');
+
+                    $('#cart > ul').load('index.php?route=common/cart/info ul li');
+                }
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+            }
+        });
+}
