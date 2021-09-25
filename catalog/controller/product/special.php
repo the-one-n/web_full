e0ee8b6d3595969cfe0390b74f610dd44c48a907
pvrvd0x <d3m35s1ah@protonmail.com>
2021-09-25 21:40:25 +0300

    Я ВИДЕЛ БОГА, ОН БЛАГОСЛОВИЛ МЕНЯ НА ЕБУЧИЕ АКЦИИ

--- catalog/controller/product/special.php
+++ catalog/controller/product/special.php
@@ -111,6 +111,41 @@ class ControllerProductSpecial extends Controller {
 				$rating = false;
 			}
 
+            $options = array();
+
+            foreach ($this->model_catalog_product->getProductOptions($result['product_id']) as $option) {
+                $product_option_value_data = array();
+
+                foreach ($option['product_option_value'] as $option_value) {
+                    if (!$option_value['subtract'] || ($option_value['quantity'] > 0)) {
+                        if ((($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) && (float)$option_value['price']) {
+                            $price = $this->currency->format($this->tax->calculate($option_value['price'], $result['tax_class_id'], $this->config->get('config_tax') ? 'P' : false), $this->session->data['currency']);
+                        } else {
+                            $price = false;
+                        }
+
+                        $product_option_value_data[] = array(
+                            'product_option_value_id' => $option_value['product_option_value_id'],
+                            'option_value_id'         => $option_value['option_value_id'],
+                            'name'                    => $option_value['name'],
+                            'image'                   => $this->model_tool_image->resize($option_value['image'], 50, 50),
+                            'price'                   => $price,
+                            'price_prefix'            => $option_value['price_prefix']
+                        );
+                    }
+                }
+
+                $options[] = array(
+                    'product_option_id'    => $option['product_option_id'],
+                    'product_option_value' => $product_option_value_data,
+                    'option_id'            => $option['option_id'],
+                    'name'                 => $option['name'],
+                    'type'                 => $option['type'],
+                    'value'                => $option['value'],
+                    'required'             => $option['required']
+                );
+            }
+
 			$data['products'][] = array(
 				'product_id'  => $result['product_id'],
 				'thumb'       => $image,
@@ -118,6 +153,7 @@ class ControllerProductSpecial extends Controller {
 				'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
 				'price'       => $price,
 				'special'     => $special,
+                'options'     => $options,
 				'tax'         => $tax,
 				'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
 				'rating'      => $result['rating'],
@@ -201,7 +237,7 @@ class ControllerProductSpecial extends Controller {
 
 		$data['limits'] = array();
 
-		$limits = array_unique(array($this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), 25, 50, 75, 100));
+		$limits = array_unique(array(5, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), 50, 75, 100));
 
 		sort($limits);
 
--- catalog/language/ru-ru/product/discount_list.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_['meta_title'] = 'Скидки';
-$_['meta_description'] = 'Страница с товарами на скидке';
-$_['meta_keyword'] = 'discounts';
